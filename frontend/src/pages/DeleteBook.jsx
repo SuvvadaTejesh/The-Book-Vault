faab8bb50fdb42ea95882176bfdb4fb3b295dc16
SuvvadaTejesh <114825077+SuvvadaTejesh@users.noreply.github.com>
2024-08-28 01:46:43 +0530

    Update DeleteBook.jsx

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`http://localhost:5555/books/${id}`)
+      .delete(`https://the-book-vault-4.onrender.com/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
